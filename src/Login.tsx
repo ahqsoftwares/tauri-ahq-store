c12c87d282baccce6d70a8906e11f656b783eca8
AHQMiness <84524025+ahqsoftwares@users.noreply.github.com>
2022-08-03 21:00:14 +0530

    Firebase Auth

---
+++ src/Login.tsx
@@ -0,0 +1,165 @@
+import {useState} from "react";
+
+function SignUp(props: any) {
+         let [email, setEmail] = useState(""),
+         [step, setStep] = useState(1),
+         [pwd, setPwd] = useState("");
+
+         function contd(event: any) {
+                  console.log(event);
+                  event.preventDefault();
+                  if (step === 3) {
+                           setStep(1);
+                  } else {
+                           setStep(step + 1);
+                  }
+         }
+
+         return (
+                  <>       
+                           <div className="mt-10"></div>
+                           <h1>Sign Up</h1>
+                           <h2>Create your new account</h2>
+
+                           <div className="mt-auto"></div>
+                           
+                           <form className="modal" onSubmit={contd}>
+                                    <div className="mt-auto"></div>
+
+                                    <input type={"email"} required={true} placeholder={"Email ID"} onChange={(e) => {
+                                             if (step === 1) {
+                                                      setEmail(e.target.value);
+                                             } else {
+                                                      e.target.value = email;
+                                             }
+                                    }}></input>
+
+                                    {step > 1 ? 
+                                             <>
+                                                      <div className="mt-[1rem]"></div>
+                                                      <input type={"password"} required={true} placeholder={"Password"} onChange={(e) => {
+                                                               if (step === 2) {
+                                                                        setPwd(e.target.value);
+                                                               } else {
+                                                                        e.target.value = pwd;
+                                                               }
+                                                      }}></input>
+                                             </>
+                                    : <></>}
+                           
+                                    <div className="mt-auto"></div>
+                                    
+                                    <button className="button">
+                                             Continue
+                                    </button>
+
+                                    <div className="mb-[1rem]"></div>
+                           </form>
+
+                           {step === 1 ?
+                           <div className="flex w-[90%]">
+                                    <button onClick={() => {
+                                             props.change("login");
+                                    }}>Login</button>
+                                    <div className="ml-auto"></div>
+                                    <button onClick={() => {
+                                             props.change("reset");
+                                    }}>
+                                             Forgot Password?
+                                    </button>
+                           </div>
+                           : <></>}
+
+                           <div className="mb-auto"></div>
+                  </>
+         );
+}
+
+
+
+
+type log = {
+         change: Function,
+         auth: any,
+         login: any
+}
+function Login(props: log) {
+         const {auth, login} = props;
+
+         let [e, setE] = useState(""),
+         [email, setEmail] = useState(""),
+         [pwd, setPwd] = useState("");
+
+         return (
+                  <>
+                           <div className="mt-10"></div>
+
+                           <h1 className="line">Welcome</h1>
+                           <h2 className="line">Login to start your journey!</h2>
+                           <h3 style={{"color": "red"}}>{e}</h3>
+
+                           <div className="mt-[15rem]"></div>
+                           
+                           <form className="modal" onSubmit={(e) => {
+                                    e.preventDefault()
+                                    login(auth, email, pwd)
+                                    .then(() => {
+                                             setE("");
+                                    })
+                                    .catch(() => {
+                                             setE("Invalid username/password");
+                                             setPwd("");
+                                    })
+                           }}>
+                                    
+                                    <input type={"email"} required={true} placeholder={"Email ID"} onChange={(e) => setEmail(e.target.value)} value={email}></input>
+                                    <div className="mt-[1rem]"></div>
+                                    <input type={"password"} required={true} placeholder={"Password"} onChange={(e) => setPwd(e.target.value)} value={pwd}></input>
+
+
+                                    <button className="button">Login</button>
+                           </form>
+
+                           <div className="mt-2"></div>
+                           
+                           <div className="flex w-[90%]">
+                                    <button onClick={() => {
+                                             props.change("signup");
+                                    }}>Create your account!</button>
+                                    <div className="ml-auto"></div>
+                                    <button onClick={() => {
+                                             props.change("reset");
+                                    }}>
+                                             Forgot Password?
+                                    </button>
+                           </div>
+
+                           <div className="mt-auto"></div>
+                  </>
+         )
+}
+
+function Init(props: any) {
+         const {create, login, verify, reset, auth} = props.data;
+         let [type, setType] = useState("login");
+         
+         return (
+                  <header className="login-background">
+                           <div className="modal">
+                                             {type === "login" ? <Login change={(page: string) => {
+                                                      setType(page);
+                                             }} login={login} auth={auth}/> : <></>}
+                                             
+                                             {type === "signup" ? 
+                                             <SignUp change={(page: string) => {
+                                                      setType(page);
+                                             }} create={create} verify={verify}/> : <></>}
+
+                                             {type === "reset" ?
+                                             <></> : <></>}
+                           </div>
+                  </header>
+         )
+}
+
+export default Init;
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,54 @@
+/*Main Modules
+*/
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './config/App';
-import Store from "./app/index";
 import reportWebVitals from './reportWebVitals';
+
+/*Tauri
+ */
 import { isPermissionGranted, requestPermission, sendNotification } from '@tauri-apps/api/notification';
 import { checkUpdate, installUpdate } from '@tauri-apps/api/updater';
 import { relaunch } from '@tauri-apps/api/process';
 
+/*Apps
+*/
+import App from './config/App';
+import Store from "./app/index";
+import Login from "./Login";
+
+/*Firebase
+*/
+import {initializeApp} from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail, verifyPasswordResetCode } from "firebase/auth";
+
+/*Global CSS
+*/
+import './index.css';
+
+/*Constants
+*/
+const config = {
+  apiKey: "AIzaSyAXAkoxKG4chIuIGHPkVG8Sma9mTJqiC84",
+  authDomain: "ahq-store.firebaseapp.com",
+  databaseURL: "https://ahq-store-default-rtdb.firebaseio.com",
+  projectId: "ahq-store",
+  storageBucket: "ahq-store.appspot.com",
+  messagingSenderId: "460016490107",
+  appId: "1:460016490107:web:50123c20ca44ccee3b74de",
+  measurementId: "G-TEZS1Y48L1"
+};
+
+const app = initializeApp(config);
+const auth = getAuth(app);
+
+/*Logic
+*/
 (async() => {
   let permissionGranted = await isPermissionGranted();
   if (!permissionGranted) {
     const permission = await requestPermission();
     permissionGranted = permission === 'granted';
-  }
-  if (permissionGranted) {
+  } else {
     sendNotification({ title: 'Development Build', body: 'Not for consumer use'});
   }
 })()
@@ -26,6 +60,7 @@ render("Checking for updates...", App);
 checkUpdate().then(async({shouldUpdate, manifest}) => {
   if (shouldUpdate) {
     render(`Update to ${manifest?.version} Available...`, App);
+
     setTimeout(async() => {
       render(`Installing ${manifest?.version}`,  App);
       setTimeout(async() => {
@@ -33,18 +68,52 @@ checkUpdate().then(async({shouldUpdate, manifest}) => {
         await relaunch();
       }, 3000);
     }, 5000);
+
   } else {
+
     render("Launching Store...", App);
-    setTimeout(async() => {
-      root.render(
-        <React.StrictMode>
-          <Store />
-        </React.StrictMode>
-      )
-    }, 2000);
+
+    if (!auth.currentUser) {
+      storeLoad(Login, {
+        create: createUserWithEmailAndPassword,
+        login: signInWithEmailAndPassword,
+        verify: sendEmailVerification,
+        reset: verifyPasswordResetCode,
+        auth
+      });
+    } else {
+      storeLoad(Store);
+    }
+
+    auth.onAuthStateChanged((user) => {
+      user ? storeLoad(Store) : storeLoad(Login, {
+        create: createUserWithEmailAndPassword,
+        login: signInWithEmailAndPassword,
+        verify: sendEmailVerification,
+        reset: verifyPasswordResetCode,
+        auth
+      });
+    });
   }
 });
 
+
+/**
+ * Load a Store Component on the DOM
+ * @param Component 
+ * @param prop 
+ */
+function storeLoad(Component: any, prop?: Object) {
+    root.render(<React.StrictMode>
+      <Component data={prop ? prop : {}}/>
+    </React.StrictMode>)
+}
+
+/**
+ * Loads updater
+ * @param {string} state 
+ * @param {React.Component} App 
+ */
 function render(state: string, App: any) {
   root.render(
     <React.StrictMode>
@@ -53,7 +122,4 @@ function render(state: string, App: any) {
   );
 }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
