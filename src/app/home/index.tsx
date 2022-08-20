e21be8ae084f7d2b71551c939959a3c9dbb7b4bc
AHQMiness <84524025+ahqsoftwares@users.noreply.github.com>
2022-08-20 21:51:46 +0530

    tests

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,13 +1,16 @@
 import {useState} from "react";
 import { invoke } from '@tauri-apps/api/tauri';
-import {BaseDirectory} from "@tauri-apps/api/fs";
+import {appDir} from "@tauri-apps/api/path";
 
 export default function Home() {
     let [status, setState] = useState("Install Simple Host Desktop (SAMPLE APP);");
          return (<button
             className={`button`}
             disabled={status.split(";")[1] === "-disabled"}
-            onClick={() => {
+            onClick={async() => {
+                let BaseDirectory = {
+                    App: await appDir()
+                };
                 setState("Downloading...;-disabled");
                 invoke("download", { url: "https://github.com/ahqsoftwares/Simple-Host-App/releases/download/v2.1.0/Simple-Host-Desktop-Setup-2.1.0.exe", path: `${BaseDirectory.App}/install/` })
 .then(() => {
