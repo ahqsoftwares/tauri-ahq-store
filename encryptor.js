2c1ebeaed1dd2901db8939f2d364a7928e999662
AHQ Miness <ahqsecret@gmail.com>
2023-06-04 17:32:31 +0530

    Store Service with everything required

--- encryptor.js
+++ encryptor.js
@@ -1,2 +1,5 @@
+const { hashSync } = require("bcrypt");
 const { writeFileSync } = require("fs");
 writeFileSync("./src-tauri/src/encrypt", `"${process.env.KEY}"`);
+writeFileSync("./src-service/src/auth/encrypt", `"${process.env.KEY}"`);
+writeFileSync("./src-service/src/auth/hash", `"${hashSync(process.env.KEY, 10)}"`);
