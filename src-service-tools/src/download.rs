fca39fd550e66e7929c796b42c17d3da2940ffb5
AHQ Miness <ahqsecret@gmail.com>
2023-07-17 14:04:03 +0530

    start if no cache hit

--- src-service-tools/src/download.rs
+++ src-service-tools/src/download.rs
@@ -1,8 +1,12 @@
+use std::fs::create_dir_all;
+
 use downloader::Downloader;
 
 pub fn download(path: &str, url: &str) {
     let url = url.clone();
     let path = path.clone();
+
+    create_dir_all(&path).unwrap_or(());
 
     let mut downloader = Downloader::builder()
         .download_folder(std::path::Path::new(path))
