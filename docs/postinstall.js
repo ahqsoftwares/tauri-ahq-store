f5a61127ae5f538ba2b5b64554ce70a8c8ede038
AHQ Miness <ahqsecret@gmail.com>
2023-08-22 16:49:03 +0530

    Update Site

--- docs/postinstall.js
+++ docs/postinstall.js
@@ -1,5 +1,6 @@
-import { writeFileSync } from "fs";
+import { mkdirSync, writeFileSync } from "fs";
 
 const verifyPage = "dh=83790a0f96cc5927e34557ec683be10f6886e078";
 
-writeFileSync("./build/verify.txt", verifyPage);
+mkdirSync("./build/.well-known");
+writeFileSync("./build/.well-known/discord", verifyPage);
