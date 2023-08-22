5d5b61a0ae497391e2d96f09334053ac74b502d7
AHQ Miness <ahqsecret@gmail.com>
2023-08-22 16:26:43 +0530

    Update Site

--- docs/src/components/Shell/Shell.tsx
+++ docs/src/components/Shell/Shell.tsx
@@ -5,27 +5,35 @@ import Resolve from "../../router/resolve";
 
 export default function Shell() {
   const [page, setPageData] = useState(<></>);
+  const [special, setSpecial] = useState(false);
 
   useEffect(() => {
-    Resolve().then((data) => {
-      setPageData(data);
+    if (document.location.pathname === "/.well-known/discord") {
+      setSpecial(true);
+      setPageData(<>dh=83790a0f96cc5927e34557ec683be10f6886e078</>);
+    } else {
+      Resolve().then((data) => {
+        setPageData(data);
 
-      const loading = document.querySelector("#loading");
-      loading?.setAttribute("class", "hidden");
-      loading?.setAttribute("id", "");
-      try {
-        (loading as Element).innerHTML = "";
-      } catch (_) {
-        //
-      }
-    });
+        const loading = document.querySelector("#loading");
+        loading?.setAttribute("class", "hidden");
+        loading?.setAttribute("id", "");
+        try {
+          (loading as Element).innerHTML = "";
+        } catch (_) {
+          //
+        }
+      });
+    }
   }, []);
 
   return (
     <>
-      <Navigation />
+      {special ? {page} : <>
+        <Navigation />
 
-      <div className="p-2 mt-2 w-full">{page}</div>
+        <div className="p-2 mt-2 w-full">{page}</div>
+      </>}
     </>
   );
 }
