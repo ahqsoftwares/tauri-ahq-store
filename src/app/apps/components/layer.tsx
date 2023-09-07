29c7f7ed6ecb547cd94446a6f6fb34a999f896e5
AHQ Miness <ahqsecret@gmail.com>
2023-09-07 20:53:05 +0530

    New Alpha build

--- src/app/apps/components/layer.tsx
+++ src/app/apps/components/layer.tsx
@@ -1,4 +1,7 @@
-export default function Splitter(props: { children: any; alt: string }) {
+export default function Splitter(props: {
+  children: JSX.Element[];
+  alt: string;
+}) {
   return (
     <div className="layer mx-[0.5rem]">
       <h1 className="title">{props.alt}</h1>
