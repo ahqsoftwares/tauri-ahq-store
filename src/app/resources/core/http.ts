ed77931e86f5aa20790386e269b21194d14a82d6
AHQ Miness <ahqsecret@gmail.com>
2023-09-07 20:54:42 +0530

    New Alpha build

--- src/app/resources/core/http.ts
+++ src/app/resources/core/http.ts
@@ -1,25 +1,32 @@
 import { invoke } from "@tauri-apps/api";
-import { FetchOptions, ResponseType, fetch as tauriFetch } from "@tauri-apps/api/http";
+import {
+  FetchOptions,
+  ResponseType,
+  fetch as tauriFetch,
+} from "@tauri-apps/api/http";
 
-export default async function fetch<T = any>(url: string, config: FetchOptions | undefined) {
-    const email = localStorage.getItem("email") as string;
+export default async function fetch<T = any>(
+  url: string,
+  config: FetchOptions | undefined,
+) {
+  const email = localStorage.getItem("email") as string;
 
-    const pwd = await invoke("decrypt", {
-        encrypted: JSON.parse(localStorage.getItem("password") || "[]") as number[],
-    }).catch(() => "a");
+  const pwd = await invoke("decrypt", {
+    encrypted: JSON.parse(localStorage.getItem("password") || "[]") as number[],
+  }).catch(() => "a");
 
-    console.log(url);
+  console.log(url);
 
-    return await tauriFetch<T>(url, {
-        responseType: ResponseType.JSON,
-        ...(config||{}),
-        method: config?.method || "GET",
-        timeout: 100,
-        headers: {
-            "User-Agent": navigator.userAgent,
-            uid: email,
-            pwd,
-            ...(config?.headers || {})
-        }
-    });
+  return await tauriFetch<T>(url, {
+    responseType: ResponseType.JSON,
+    ...(config || {}),
+    method: config?.method || "GET",
+    timeout: 100,
+    headers: {
+      "User-Agent": navigator.userAgent,
+      uid: email,
+      pwd,
+      ...(config?.headers || {}),
+    },
+  });
 }
