86128dcff55b6e79d406d1b0531ab32758276070
AHQ Miness <ahqsecret@gmail.com>
2023-09-30 23:15:36 +0530

    Service

--- src-service-alpha/src/authentication.rs
+++ src-service-alpha/src/authentication.rs
@@ -1,35 +1,42 @@
+#[cfg(not(debug_assertions))]
 use sysinfo::{Pid, ProcessExt, System, SystemExt};
 
+#[cfg(not(debug_assertions))]
 use crate::utils::*;
 
 #[cfg_attr(debug_assertions, allow(dead_code))]
 pub fn authenticate_process(pid: usize) -> bool {
     #[cfg(not(debug_assertions))]
-    let exe = String::from_utf8_lossy(
-        format!(
-            "{}\\Program Files\\AHQ Store\\AHQ Store.exe",
-            get_main_drive()
-        )
-        .as_bytes(),
-    );
+    {
+        let exe = String::from_utf8_lossy(
+            format!(
+                "{}\\Program Files\\AHQ Store\\AHQ Store.exe",
+                get_main_drive()
+            )
+            .as_bytes(),
+        );
 
-    #[cfg(debug_assertions)]
-    let exe = String::from_utf8_lossy(br#"E:\rust\tokio-client\target\debug\tokio-client.exe"#);
+        let mut system = System::new_all();
+        system.refresh_all();
 
-    let mut system = System::new_all();
-    system.refresh_all();
+        let process = system.process(Pid::from(pid));
 
-    let process = system.process(Pid::from(pid));
+        if let Some(process) = process {
+            let exe_path = process.exe().as_os_str().to_string_lossy();
 
-    if let Some(process) = process {
-        let exe_path = process.exe().as_os_str().to_string_lossy();
+            let running_for_secs = now() - process.start_time();
 
-        let running_for_secs = now() - process.start_time();
-
-        if exe_path == exe && running_for_secs < 2 {
-            return true;
+            if exe_path == exe && running_for_secs < 2 {
+                return true;
+            }
         }
+
+        false
     }
 
-    false
+    #[cfg(debug_assertions)]
+    {
+        let _ = pid;
+        true
+    }
 }
