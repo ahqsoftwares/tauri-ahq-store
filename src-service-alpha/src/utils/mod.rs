8e9f207c4576a8448ab3133fd28af14852b49e7c
AHQ Miness <ahqsecret@gmail.com>
2023-10-01 21:41:10 +0530

    Updates\nAdd :\GitHub\ahq-store-tauri\src-service-alpha\src\utils\mod.rs

--- src-service-alpha/src/utils/mod.rs
+++ src-service-alpha/src/utils/mod.rs
@@ -1,6 +1,6 @@
 use std::{
-    net::TcpListener,
-    time::{SystemTime, UNIX_EPOCH},
+  net::TcpListener,
+  time::{SystemTime, UNIX_EPOCH},
 };
 
 mod db;
@@ -11,26 +11,57 @@ pub use log_file::*;
 
 pub mod structs;
 
+pub fn get_program_folder(app_id: &str) -> String {
+  format!(
+    "{}\\ProgramData\\AHQ Store Applications\\Programs\\{}",
+    &get_main_drive(),
+    &app_id
+  )
+}
+
+pub fn get_programs() -> String {
+  format!(
+    "{}\\ProgramData\\AHQ Store Applications\\Programs",
+    &get_main_drive()
+  )
+}
+
+pub fn get_installer_file(app_id: &str) -> String {
+  format!(
+    "{}\\ProgramData\\AHQ Store Applications\\Installers\\{}.zip",
+    &get_main_drive(),
+    &app_id
+  )
+}
+
+pub fn get_target_lnk(name: &str) -> String {
+  format!(
+    "{}\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\AHQ Store\\{}.lnk",
+    &get_main_drive(),
+    &name
+  )
+}
+
 pub fn now() -> u64 {
-    SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_secs()
+  SystemTime::now()
+    .duration_since(UNIX_EPOCH)
+    .unwrap()
+    .as_secs()
 }
 
 pub fn get_available_port() -> Option<u16> {
-    (49152..65535).find(|port| port_is_available(*port))
+  (49152..65535).find(|port| port_is_available(*port))
 }
 
 fn port_is_available(port: u16) -> bool {
-    match TcpListener::bind(("127.0.0.1", port)) {
-        Ok(_) => true,
-        Err(_) => false,
-    }
+  match TcpListener::bind(("127.0.0.1", port)) {
+    Ok(_) => true,
+    Err(_) => false,
+  }
 }
 
 pub fn get_main_drive() -> String {
-    std::env::var("SystemDrive")
-        .unwrap_or("C:".into())
-        .to_string()
+  std::env::var("SystemDrive")
+    .unwrap_or("C:".into())
+    .to_string()
 }
