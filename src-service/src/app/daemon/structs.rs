757b364e1b1d8bc9cd39f1ecb605a5f7eb9f309f
AHQ Miness <ahqsecret@gmail.com>
2023-10-01 21:40:14 +0530

    New Updates\nAdd :\GitHub\ahq-store-tauri\src-service\src\app\daemon\structs.rs

--- src-service/src/app/daemon/structs.rs
+++ src-service/src/app/daemon/structs.rs
@@ -2,20 +2,20 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct GlobalPreferences {
-    launch_app: bool,
-    install_apps: bool,
+  launch_app: bool,
+  install_apps: bool,
 }
 
 impl Default for GlobalPreferences {
-    fn default() -> Self {
-        Self {
-            install_apps: true,
-            launch_app: true,
-        }
+  fn default() -> Self {
+    Self {
+      install_apps: true,
+      launch_app: true,
     }
+  }
 }
 
 pub enum Preferences {
-    Struct(GlobalPreferences),
-    Data(String),
+  Struct(GlobalPreferences),
+  Data(String),
 }
