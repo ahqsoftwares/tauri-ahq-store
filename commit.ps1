6c638141c05922f68c850980e9cfc4d17c98fa67
AHQ Miness <ahqsecret@gmail.com>
2023-10-01 21:37:42 +0530

    New Updates\nAdd :\GitHub\ahq-store-tauri\commit.ps1

---
+++ commit.ps1
@@ -0,0 +1,11 @@
+$commitMessage = Read-Host "Enter a commit message"
+
+$files = Get-ChildItem -Recurse -File
+
+foreach ($file in $files) {
+    git add $file.FullName
+
+    $relativePath = $file.FullName.Substring($rootDirectory.Length + 1)
+    $cmtMessage = $commitMessage + '\n' + "Add $relativePath"
+    git commit -m $cmtMessage
+}
