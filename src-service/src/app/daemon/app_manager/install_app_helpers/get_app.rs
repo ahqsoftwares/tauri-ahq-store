b83b094b90681af304f7dfa118030de73a58f125
AHQ Miness <ahqsecret@gmail.com>
2023-10-01 21:40:26 +0530

    New Updates\nAdd :\GitHub\ahq-store-tauri\src-service\src\app\daemon\app_manager\install_app_helpers\get_app.rs

--- src-service/src/app/daemon/app_manager/install_app_helpers/get_app.rs
+++ src-service/src/app/daemon/app_manager/install_app_helpers/get_app.rs
@@ -4,25 +4,26 @@ use crate::app::daemon::app_manager::{get_app, AppDownloaded};
 
 #[allow(dead_code)]
 pub fn get_apps_download_url(
-    apps: Vec<String>,
-    client: Client,
-    commit_id: String,
+  apps: Vec<String>,
+  client: Client,
+  commit_id: String,
 ) -> Vec<AppDownloaded> {
-    let apps = get_app::get_apps(apps, client, commit_id);
+  let apps = get_app::get_apps(apps, client, commit_id);
 
-    apps.iter()
-        .map(|app| {
-            let url = app.app.download.clone();
-            let exec = app.app.exe.clone();
-            let name = app.app.title.clone();
-            let version = app.app.version.clone();
+  apps
+    .iter()
+    .map(|app| {
+      let url = app.app.download.clone();
+      let exec = app.app.exe.clone();
+      let name = app.app.title.clone();
+      let version = app.app.version.clone();
 
-            AppDownloaded {
-                url,
-                exec,
-                name,
-                version,
-            }
-        })
-        .collect()
+      AppDownloaded {
+        url,
+        exec,
+        name,
+        version,
+      }
+    })
+    .collect()
 }
