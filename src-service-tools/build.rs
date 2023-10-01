d65d1041fec2b54ede3d47e59fa4ab175267a329
AHQ Miness <ahqsecret@gmail.com>
2023-10-01 21:43:14 +0530

    Updates

--- src-service-tools/build.rs
+++ src-service-tools/build.rs
@@ -1,6 +1,6 @@
 use {std::io, winres::WindowsResource};
 
 fn main() -> io::Result<()> {
-    WindowsResource::new().set_icon("src/icon.ico").compile()?;
-    Ok(())
+  WindowsResource::new().set_icon("src/icon.ico").compile()?;
+  Ok(())
 }
