932286d36b662a32852dd3c7e767086fd5b60cc3
AHQ Miness <ahqsecret@gmail.com>
2023-10-01 21:40:09 +0530

    New Updates\nAdd :\GitHub\ahq-store-tauri\src-service\src\app\mod.rs

--- src-service/src/app/mod.rs
+++ src-service/src/app/mod.rs
@@ -3,21 +3,21 @@ use reqwest::blocking::Client;
 pub mod daemon;
 
 pub use daemon::{
-    get_apps, get_commit_id, get_update_stats, install_apps, list_apps, post_preferences,
-    preferences, run_update, set_sender, uninstall_apps,
+  get_apps, get_commit_id, get_update_stats, install_apps, list_apps, post_preferences,
+  preferences, run_update, set_sender, uninstall_apps,
 };
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 struct Commit {
-    sha: String,
+  sha: String,
 }
 
 pub fn init() {
-    let client = Client::new();
+  let client = Client::new();
 
-    daemon::start(client.clone());
+  daemon::start(client.clone());
 }
 
 pub fn stop() {
-    daemon::stop();
+  daemon::stop();
 }
