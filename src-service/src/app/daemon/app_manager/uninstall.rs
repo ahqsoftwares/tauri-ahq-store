8dd2da37c5d32567b6c8046b9839632078f5e8bc
AHQ Miness <ahqsecret@gmail.com>
2023-10-01 21:40:21 +0530

    New Updates\nAdd :\GitHub\ahq-store-tauri\src-service\src\app\daemon\app_manager\uninstall.rs

--- src-service/src/app/daemon/app_manager/uninstall.rs
+++ src-service/src/app/daemon/app_manager/uninstall.rs
@@ -3,20 +3,20 @@ use reqwest::blocking::Client;
 use super::install_app_helpers::{get_apps_download_url, uninstall_app};
 
 pub fn uninstall(apps: Vec<String>, commit_id: String, client: Client) -> Vec<String> {
-    let mut payload = vec![];
-    let urls = get_apps_download_url(apps.clone(), client.clone(), commit_id.clone());
+  let mut payload = vec![];
+  let urls = get_apps_download_url(apps.clone(), client.clone(), commit_id.clone());
 
-    let mut i = 0;
-    for url in urls {
-        let id = &apps[i];
+  let mut i = 0;
+  for url in urls {
+    let id = &apps[i];
 
-        match uninstall_app(id, url) {
-            1 => payload.push(id.into()),
-            _ => {}
-        }
-
-        i += 1;
+    match uninstall_app(id, url) {
+      1 => payload.push(id.into()),
+      _ => {}
     }
 
-    payload
+    i += 1;
+  }
+
+  payload
 }
