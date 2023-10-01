098dfa946851945b7fb545b6c80fe4cf9a8ad79f
AHQ Miness <ahqsecret@gmail.com>
2023-10-01 21:41:00 +0530

    Updates\nAdd :\GitHub\ahq-store-tauri\src-service-alpha\src\authentication.rs

--- src-service-alpha/src/authentication.rs
+++ src-service-alpha/src/authentication.rs
@@ -6,37 +6,37 @@ use crate::utils::*;
 
 #[cfg_attr(debug_assertions, allow(dead_code))]
 pub fn authenticate_process(pid: usize) -> bool {
-    #[cfg(not(debug_assertions))]
-    {
-        let exe = String::from_utf8_lossy(
-            format!(
-                "{}\\Program Files\\AHQ Store\\AHQ Store.exe",
-                get_main_drive()
-            )
-            .as_bytes(),
-        );
+  #[cfg(not(debug_assertions))]
+  {
+    let exe = String::from_utf8_lossy(
+      format!(
+        "{}\\Program Files\\AHQ Store\\AHQ Store.exe",
+        get_main_drive()
+      )
+      .as_bytes(),
+    );
 
-        let mut system = System::new_all();
-        system.refresh_all();
+    let mut system = System::new_all();
+    system.refresh_all();
 
-        let process = system.process(Pid::from(pid));
+    let process = system.process(Pid::from(pid));
 
-        if let Some(process) = process {
-            let exe_path = process.exe().as_os_str().to_string_lossy();
+    if let Some(process) = process {
+      let exe_path = process.exe().as_os_str().to_string_lossy();
 
-            let running_for_secs = now() - process.start_time();
+      let running_for_secs = now() - process.start_time();
 
-            if exe_path == exe && running_for_secs < 2 {
-                return true;
-            }
-        }
-
-        false
+      if exe_path == exe && running_for_secs < 2 {
+        return true;
+      }
     }
 
-    #[cfg(debug_assertions)]
-    {
-        let _ = pid;
-        true
-    }
+    false
+  }
+
+  #[cfg(debug_assertions)]
+  {
+    let _ = pid;
+    true
+  }
 }
