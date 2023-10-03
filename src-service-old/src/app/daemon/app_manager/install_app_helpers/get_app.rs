907072620a9f86fd3bd7167549e5cc8060072f29
AHQ Miness <ahqsecret@gmail.com>
2023-10-03 20:26:07 +0530

    Updates     Add :\GitHub\ahq-store-tauri\src-service-old\src\app\daemon\app_manager\install_app_helpers\get_app.rs

---
+++ src-service-old/src/app/daemon/app_manager/install_app_helpers/get_app.rs
@@ -0,0 +1,29 @@
+use reqwest::blocking::Client;
+
+use crate::app::daemon::app_manager::{get_app, AppDownloaded};
+
+#[allow(dead_code)]
+pub fn get_apps_download_url(
+  apps: Vec<String>,
+  client: Client,
+  commit_id: String,
+) -> Vec<AppDownloaded> {
+  let apps = get_app::get_apps(apps, client, commit_id);
+
+  apps
+    .iter()
+    .map(|app| {
+      let url = app.app.download.clone();
+      let exec = app.app.exe.clone();
+      let name = app.app.title.clone();
+      let version = app.app.version.clone();
+
+      AppDownloaded {
+        url,
+        exec,
+        name,
+        version,
+      }
+    })
+    .collect()
+}
