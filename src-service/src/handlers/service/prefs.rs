0f4cb67a532b7be8bdebc42f7a990fee109f5d1f
AHQ Miness <ahqsecret@gmail.com>
2023-10-07 23:20:33 +0530

    Fixes

--- src-service/src/handlers/service/prefs.rs
+++ src-service/src/handlers/service/prefs.rs
@@ -3,14 +3,14 @@ use crate::utils::get_main_drive;
 
 static _PREFS: &str = "{root}\\ProgramData\\AHQ Store Applications\\perfs.encryped";
 
-pub fn get() {
+pub fn _get() {
   /*let path = PATH.replace("{}", &get_main_drive());
   
   Prefs::str_to(Prefs::get(&vector).ok()?).ok()*/
   todo!();
 }
 
-pub fn set(_: &str) {
+pub fn _set(_: &str) {
   /*let path = PATH.replace("{}", &get_main_drive());
   
   let data = (Prefs::str_to(values).ok()?).convert().ok()?;*/
