aa24eb8529800e01cefa8a27fe0ade9eecca0e9d
AHQ Miness <ahqsecret@gmail.com>
2023-11-13 10:41:34 +0530

    No Fuss

--- src-tauri/src/windows/ws.rs
+++ src-tauri/src/windows/ws.rs
@@ -7,7 +7,7 @@ use std::{
 };
 use tungstenite::{connect, stream::MaybeTlsStream, Message, WebSocket};
 
-use crate::windows::{encryption::decrypt, get_system_dir};
+use crate::{encryption::decrypt, windows::get_system_dir};
 
 static mut WS: Option<WebSocket<MaybeTlsStream<TcpStream>>> = None;
 
